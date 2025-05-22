e2b79e57712bdb6dd842fc041c6b3b6090446791
苍旻 <55022033+SomiaWhiteRing@users.noreply.github.com>
2025-05-22 10:27:57 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2055933033987947"
+     crossOrigin="anonymous"></script>
+      </head>
       <body>
         {children}
         <Analytics />
